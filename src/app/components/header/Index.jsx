f1808290534df91e3b116a7eb02e945ff38a9b72
sreenesh <sreenshreddy52@gmail.com>
2024-08-31 19:08:49 +0530

    renamed dirs

--- src/app/components/header/Index.jsx
+++ src/app/components/header/Index.jsx
@@ -3,13 +3,13 @@ import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import styles from './style.module.scss';
 import { usePathname } from 'next/navigation';
 import { AnimatePresence } from 'framer-motion';
-import Nav from './nav/Index';
+import Nav from './nav/Nav';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Rounded from '../../common/RoundedButton';
 import Magnetic from '../../common/Magnetic';
 
-export default function Nav() {
+export default function Index() {
     const header = useRef(null);
     const [isActive, setIsActive] = useState(false);
     const pathname = usePathname();
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import Header from './components/header/Nav';
+import Header from './components/header/nav/Nav';
 import * as React from 'react'; // Import React
 
 const inter = Inter({ subsets: ['latin'] })
