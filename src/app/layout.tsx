fa358d7e0a9a1ba8e746f61bf1eefb0ae6e895ba
sreenesh <sreenshreddy52@gmail.com>
2024-09-01 21:29:08 +0530

    checking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import Header from './components/Header/Header';
+import Header from './components/Header/Header'
 import * as React from 'react'; // Import React
 
 const inter = Inter({ subsets: ['latin'] })
